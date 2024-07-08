1cfa96f1f70760d47d2267ca1ffa9cee7c6e05b1
TobiasMastroberardini <tmastroberardini1@gmail.com>
2024-07-08 18:46:50 -0300

    change $action

--- router.php
+++ router.php
@@ -10,7 +10,7 @@ require_once "./app/controllers/pageController.php";
 
 define('BASE_URL', '//' . $_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT'] . dirname($_SERVER['PHP_SELF']) . '/');
 
-$action = 'courses';
+$action = 'home';
 
 if (!empty($_GET['action'])) {
     $action = $_GET['action'];
